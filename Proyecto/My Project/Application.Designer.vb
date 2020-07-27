e69fb5491d4a00960566ab5e1a53723573df9410
Miguel <miguel.alvarado0807@gmail.com>
2020-07-26 21:30:33 -0600

    Implementado V4X4

--- Proyecto/My Project/Application.Designer.vb
+++ Proyecto/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Proyecto._4_litros
+            Me.MainForm = Global.Proyecto.V4X4
         End Sub
     End Class
 End Namespace
